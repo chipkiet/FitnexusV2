45c8f8f5c327d060ccbf1c90c4ed727cb8aba9f6
chipkier <hoccuakiet@gmail.com>
2025-10-20 00:51:14 +0700

    update apibackend for plan

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -4,5 +4,5 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
